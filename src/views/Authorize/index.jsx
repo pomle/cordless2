3968b38eac075c7cfff3141e8279723b57111acb
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-04 23:19:01 +0100

    Tweak session storage.

--- src/views/Authorize/index.jsx
+++ src/views/Authorize/index.jsx
@@ -3,19 +3,43 @@ import { parse } from 'query-string';
 
 import { createAuthorizationURL } from 'vendor/Spotify';
 
+const STORAGE_KEY = 'session';
+
+function getSession() {
+  try {
+    const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    const now = new Date().getTime();
+    if (data.expiresAt > now) {
+      return null;
+    }
+    return data.session;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+function putSession(session) {
+  const data = {
+    expiresAt: new Date().getTime() + parseFloat(session.expires_in),
+    session,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
 export class Authorize extends Component {
   constructor(props) {
     super(props);
 
-    let token = localStorage.getItem('access_token');
-    if (token.length < 30) {
-      const params = parse(props.route.location.hash);
-      token = params.access_token || '';
-      localStorage.setItem('access_token', token);
+    let session = getSession();
+    if (!session) {
+      session = parse(props.route.location.hash);
+      putSession(session);
     }
 
     this.state = {
-      token,
+      token: session.access_token,
     };
   }
 
