02f9c58a4189457b219308a385c4275197ad3337
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-04 09:09:38 +0100

    Add basic Spotify building blocks.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+
+import {createAuthorizationURL} from 'vendor/Spotify';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <a href={createAuthorizationURL('a7cf3dcdfbd64bd5ac8d960caabbc890')}>Authorize</a>
       </div>
     );
   }
--- src/vendor/Spotify/index.js
+++ src/vendor/Spotify/index.js
@@ -1,4 +1,39 @@
-window.onSpotifyWebPlaybackSDKReady = () => {
-    console.log('ready');
-  // You can now initialize Spotify.Player and use the SDK
-};
+const playerPromise = new Promise(resolve => {
+    window.onSpotifyWebPlaybackSDKReady = () => {
+        console.log('ready');
+        resolve(window.Spotify);
+    };
+});
+
+export async function getSpotify() {
+    return playerPromise;
+}
+
+export async function createPlayer() {
+    const Spotify = await getSpotify();
+    const player = new Spotify.Player({
+        name: "Cordless",
+        getOAuthToken: callback => {
+            // Run code to get a fresh access token
+            callback("access token here");
+        },
+        volume: 0.5
+    });
+    return player;
+}
+
+export function createAuthorizationURL(clientId) {
+  return 'https://accounts.spotify.com/authorize?' +
+    [
+      ['client_id', clientId],
+      ['redirect_uri', 'http://localhost:3000/spotify/auth-callback'],
+      ['response_type', 'token'],
+      [
+        'scope',
+        [
+          'user-read-private',
+          'streaming',
+        ].join(' ')
+      ]
+    ].map(([key, value]) => `${key}=${encodeURIComponent(value)}`).join('&');
+}
