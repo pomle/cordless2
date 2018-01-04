f75ce72b2ef528712fffbb15346e8a83f1dcf0bd
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-04 10:52:56 +0100

    Player and state scaffolding.

--- src/views/Player/index.jsx
+++ src/views/Player/index.jsx
@@ -1,8 +1,37 @@
 import React, { Component } from 'react';
 
+import {createPlayer} from 'vendor/Spotify';
+import {PlayerState} from './state.js';
+
 export class Player extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      player: new PlayerState(),
+    };
+  }
+
+  async componentDidMount() {
+    this.player = await createPlayer(this.props.token);
+
+    const result = await this.player.connect();
+
+    this.update(player => player.set('connected', true));
+
+    this.player.on('ready', message => {
+      this.update(player => player.onMessage({type: 'ready', message}));
+    });
+  }
+
+  update(fn) {
+    this.setState({
+      player: fn(this.state.player),
+    });
+  }
+
   render() {
-    console.log(this.props);
+    console.log(this.state.player);
 
 
     return <div>Player</div>;
