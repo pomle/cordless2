89ac1192ef40a2b8318ff3c5eea6eeb102eb9253
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-06 11:43:56 +0100

    Populate playlist meta in Playlist fragment.

--- src/fragments/Playlist/index.jsx
+++ src/fragments/Playlist/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import {Image} from 'fragments/Image';
+
 export class Playlist extends Component {
   play = () => {
     const { playbackAPI, playlist, player } = this.props;
@@ -9,15 +11,34 @@ export class Playlist extends Component {
 
   render() {
     const { playlist } = this.props;
+    const {owner} = playlist;
+    console.log(playlist);
 
     return (
       <div className="Playlist">
-        <Link
-          to={`/playlist/${playlist.id}/user/${playlist.owner.id}`}
-          className="name"
-        >
-          {playlist.name}
-        </Link>
+        <div className="image">
+          <Image candidates={playlist.images}/>
+        </div>
+
+        <div className="name">
+          <Link
+            to={`/playlist/${playlist.id}/user/${playlist.owner.id}`}
+            className="name"
+          >
+            {playlist.name}
+          </Link>
+        </div>
+
+        <div className="trackCount">
+          {playlist.tracks.total}
+        </div>
+
+        <div className="owner">
+          <Link to={`/user/${owner.id}`}>
+            {owner.display_name}
+          </Link>
+        </div>
+
         <button onClick={this.play}>Play</button>
       </div>
     );
