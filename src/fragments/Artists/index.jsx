cb56a9e5167150eb6b8da80d5929f719f61400d5
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-08 20:17:24 +0100

    Use artist.name for key.

--- src/fragments/Artists/index.jsx
+++ src/fragments/Artists/index.jsx
@@ -10,7 +10,7 @@ export class Artists extends Component {
     return (
       <div className="Artists">
         {artists.map(artist => {
-          return <div key={artist.id} className="artist">
+          return <div key={artist.id || artist.name} className="artist">
             { artist.uri
               ? <Link to={`/artist/${artist.uri.split(':')[2]}`}>{artist.name}</Link>
               : artist.name
