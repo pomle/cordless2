0fd15e27d9d6cdeacb9c737d12e15644d0289914
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-08 21:27:24 +0100

    Image fix.

--- src/fragments/Image/index.jsx
+++ src/fragments/Image/index.jsx
@@ -3,26 +3,14 @@ import React, { Component } from 'react';
 import './Image.css';
 
 export class Image extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-        imageURL: props.candidates.length
-          ? props.candidates[0].url
-          : 'http://mattislist.com/marketingapp/postimage/noimageavailable.png'
-    };
-  }
-
-  componentDidMount() {
-    this.setState({loaded: true});
-  }
-
   render() {
-    const {imageURL, loaded} = this.state;
+    const {candidates} = this.props;
+    const imageURL = candidates.length
+          ? candidates[0].url
+          : 'http://mattislist.com/marketingapp/postimage/noimageavailable.png';
 
     const style = {
         backgroundImage: `url(${imageURL})`,
-        opacity: loaded ? 1 : 0,
     };
 
     return (
