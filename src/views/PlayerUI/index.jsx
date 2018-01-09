b18911e2b784047846c872cc504bd2581d62588c
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-09 21:20:03 +0100

    What is wrong with scrolling?

--- src/views/PlayerUI/index.jsx
+++ src/views/PlayerUI/index.jsx
@@ -6,12 +6,22 @@ import {Playback} from './Playback';
 import './PlayerUI.css';
 
 export class PlayerUI extends Component {
+
+  onScroll = (event) => {
+    console.log(event);
+  }
+
+  componentDidMount() {
+    this.viewport.addEventListener('scroll', this.onScroll);
+    this.viewport.addEventListener('mousewheel', this.onScroll);
+  }
+
   render() {
     const { applicationState } = this.props;
 
     return (
       <div className="PlayerUI">
-        <div className="viewport">
+        <div className="viewport" ref={node => this.viewport = node}>
           <Navigation applicationState={applicationState}/>
         </div>
         <Playback player={applicationState.player} playbackAPI={applicationState.playbackAPI}/>
