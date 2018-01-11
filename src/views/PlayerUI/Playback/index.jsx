c535c2b53182d744f59919b161705c11b998e277
Pontus Persson <pontus@warpinmedia.com>
2018-01-11 14:48:22 +0100

    Display time.

--- src/views/PlayerUI/Playback/index.jsx
+++ src/views/PlayerUI/Playback/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import {Time} from 'components/Time';
+
 import {Interface} from './Interface';
 import {NowPlaying} from './NowPlaying';
 import {Scrubber} from './Scrubber';
@@ -40,6 +42,9 @@ export class Playback extends Component {
         <Interface prev={this.prev} next={this.next} toggle={this.toggle}/>
         <div className="vis">
           <Link to="/now-playing">Now Playing</Link>
+          <div className="time">
+            <Time seconds={context.position / 1000}/>
+          </div>
         </div>
       </div>
     );
