727de50c7fb000c8eb7ae8b3c8ee08f35e029438
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-13 00:03:04 +0100

    Load Audio features.

--- src/layers/Visuals/index.jsx
+++ src/layers/Visuals/index.jsx
@@ -12,12 +12,13 @@ export class Visuals extends Component {
   constructor(props) {
     super(props);
 
-    this.onAlbumChange = onChange(compareObjectURIs, this.onAlbumChange);
-    this.onTrackChange = onChange(compareObjectURIs, this.onTrackChange);
+    this.onAlbumChange = onChange(compareObjectURIs, this.onAlbumChange.bind(this));
+    this.onTrackChange = onChange(compareObjectURIs, this.onTrackChange.bind(this));
 
     this.state = {
       track: null,
       album: null,
+      features: null,
       pulse: 0.3,
     };
   }
@@ -31,15 +32,25 @@ export class Visuals extends Component {
     }
   }
 
-  onAlbumChange = album => {
+  onAlbumChange(album) {
     this.setState({ album });
   };
 
-  onTrackChange = async track => {
+  onTrackChange(track) {
     this.setState({ track });
 
     this.onAlbumChange(track.album);
 
+    this.updateAnalyser(track.id);
+    this.updateFeatures(track.id);
+  };
+
+  async updateFeatures(trackId) {
+    const features = await this.props.trackAPI.getAudioFeatures(trackId);
+    this.setState({features});
+  }
+
+  async updateAnalyser(trackId) {
     if (this.analyzer) {
       this.analyzer.stop();
       this.analyzer = null;
@@ -48,7 +59,7 @@ export class Visuals extends Component {
     const lookAtSegment = lookAt('loudness_max', data => console.log('Segment', data.loudness_max));
     const lookAtSection = lookAt('start', data => console.log('Section', data));
 
-    const data = await this.props.trackAPI.getAudioAnalysis(track.id);
+    const data = await this.props.trackAPI.getAudioAnalysis(trackId);
     this.analyzer = analysis.stream(data);
     this.analyzer.on('data', data => {
       if (data.beat) {
@@ -64,7 +75,7 @@ export class Visuals extends Component {
       lookAtSection(data.section);
     });
     this.analyzer.start();
-  };
+  }
 
   render() {
     const { promote } = this.props;
