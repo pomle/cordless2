ab849dc2bcb9fbf9eff567aac66cc09b8de1724f
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-13 10:05:59 +0100

    Add LRU cache.

---
+++ src/library/__tests__/cache.test.js
@@ -0,0 +1,50 @@
+import {LRU} from '../cache.js';
+
+describe('LRU Cache', () => {
+  it('stores and retrieves an item', () => {
+    const lru = new LRU();
+    const item = {};
+    lru.add('a', item);
+    expect(lru.get('a')).toBe(item);
+  });
+
+  it('expells last item on overflow', () => {
+    const itemA = {};
+    const itemB = {};
+    const itemC = {};
+    const itemD = {};
+
+    const lru = new LRU(3);
+    lru.add('a', itemA);
+    lru.add('b', itemB);
+    lru.add('c', itemC);
+    expect(lru.get('a')).toBe(itemA);
+    expect(lru.get('b')).toBe(itemB);
+    expect(lru.get('c')).toBe(itemC);
+    lru.add('d', itemD);
+    expect(lru.get('a')).toBe(undefined);
+  });
+
+  it('upgrades touched items on replace', () => {
+    const itemA = {};
+    const itemB = {};
+    const itemC = {};
+    const itemD = {};
+
+    const lru = new LRU(3);
+    lru.add('a', itemA);
+    lru.add('b', itemB);
+    lru.add('c', itemC);
+    lru.add('a', itemA);
+    expect(lru.get('a')).toBe(itemA);
+    expect(lru.get('b')).toBe(itemB);
+    expect(lru.get('c')).toBe(itemC);
+    lru.add('d', itemD);
+    expect(lru.get('a')).toBe(itemA);
+    expect(lru.get('c')).toBe(itemC);
+    expect(lru.get('d')).toBe(itemD);
+    expect(lru.get('b')).toBe(undefined);
+  });
+});
+
+
---
+++ src/library/cache.js
@@ -0,0 +1,27 @@
+export class LRU {
+    constructor(limit = 1000) {
+        this.limit = limit;
+        this.items = new Map();
+    }
+
+    add(key, value) {
+        // Delete key if exists to leverage Map's internal order bump.
+        if (this.items.has(key)) {
+            this.items.delete(key);
+        }
+
+        this.items.set(key, value);
+
+        let overflow = this.items.size - this.limit;
+        for (const key of this.items.keys()) {
+            if (overflow-- < 1) {
+                break;
+            }
+            this.items.delete(key);
+        }
+    }
+
+    get(key) {
+        return this.items.get(key);
+    }
+}
