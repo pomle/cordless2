310a4542981c78d000f850201b24307af4a1941b
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-14 10:45:13 +0100

    Tweak welcome page.

--- src/views/Welcome/index.jsx
+++ src/views/Welcome/index.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import {Splash} from 'components/Splash';
-
 import CordlessLogo from 'assets/cordless-logo.png';
 import SpotifyLogo from './spotify.svg';
 
@@ -12,30 +10,28 @@ export class Welcome extends Component {
     const { authURL } = this.props;
 
     return <div className="Welcome">
-      <Splash>
-        <h1><img alt="Cordless Logo" src={CordlessLogo}/></h1>
+      <h1><img alt="Cordless Logo" src={CordlessLogo}/></h1>
 
-        <h2>
-          Open Source&ensp;<a href="https://www.spotify.com">
-            <img alt="Spotify" style={{ height: '2em' }} src={SpotifyLogo} />
-          </a>&ensp;Player
-        </h2>
+      <h2>
+        Open Source&ensp;<a href="https://www.spotify.com">
+          <img alt="Spotify" style={{ height: '2em' }} src={SpotifyLogo} />
+        </a>&ensp;Player
+      </h2>
 
-        <div className="authorize">
-          <a href={authURL}>
-            <button>Authorize</button>
-          </a>
+      <div className="authorize">
+        <a href={authURL}>
+          <button>Authorize</button>
+        </a>
+      </div>
+
+      <div className="join">
+        Join development
+        <div>
+          <a href="https://github.com/pomle/cordless2">GitHub</a>
+          &emsp;
+          <a href="https://discord.gg/kc9RRcC">Discord</a>
         </div>
-
-        <div className="join">
-          Join development
-          <div>
-            <a href="https://github.com/pomle/cordless2">GitHub</a>
-            &emsp;
-            <a href="https://discord.gg/kc9RRcC">Discord</a>
-          </div>
-        </div>
-      </Splash>
+      </div>
     </div>;
   }
 }
