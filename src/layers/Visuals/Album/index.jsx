e8d48ddaa051a25a8f39f8d21d6458e05e23a79b
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-14 20:08:15 +0100

    Tweak Album layer.

--- src/layers/Visuals/Album/index.jsx
+++ src/layers/Visuals/Album/index.jsx
@@ -1,8 +1,9 @@
 import React, { PureComponent } from 'react';
 import { Motion, spring } from 'react-motion';
 import { Surface } from 'gl-react-dom';
-//import anime from 'animejs';
+import anime from 'animejs';
 
+import { largest } from 'library/image';
 import { Renderer3D } from 'components/Renderer3D';
 
 
@@ -13,8 +14,8 @@ import {loadImage} from 'library/image.js';
 const THREE = window.THREE;
 
 const resolution = {
-  x: 800,
-  y: 450,
+  x: 1280,
+  y: 720,
 };
 
 export class Album extends PureComponent {
@@ -22,12 +23,12 @@ export class Album extends PureComponent {
     super(props);
 
     this.scene = new THREE.Scene();
-    this.scene.add(new THREE.AmbientLight(0x909090));
-    const light = new THREE.PointLight(0xffffff, 1, 100);
-    light.position.set(0, 0, 20);
-    this.scene.add(light);
+    this.scene.add(new THREE.AmbientLight(0xffffff));
+    //const light = new THREE.PointLight(0xffffff, 1, 100);
+    //light.position.set(0, 0, 20);
+    //this.scene.add(light);
     this.camera = new THREE.PerspectiveCamera(
-      50,
+      40,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
@@ -35,17 +36,33 @@ export class Album extends PureComponent {
     this.camera.position.z = 30;
 
     this.image = null;
-    this.albums = [];
+    this.albums = new Set();
   }
 
   componentWillReceiveProps({ album }) {
-    const image = album && album.images[0].url;
+    const image = album && largest(album.images).url;
 
     if (this.image === image) {
       return;
     }
 
     this.image = image;
+
+    this.albums.forEach(album => {
+      anime({
+        targets: [album.position, album.material],
+        z: 10,
+        y: 20,
+        opacity: 0,
+        duration: 1000,
+        easing: 'easeInQuad',
+        complete: () => {
+          this.scene.remove(album);
+        }
+      });
+
+      this.albums.delete(album);
+    });
 
     loadImage(image)
       .then(image => {
@@ -63,16 +80,43 @@ export class Album extends PureComponent {
           })
         );
 
-        album.userData.update = (diff, time) => {
-          album.rotation.y = Math.sin(time / 10000) * 1;
-        };
-
-        this.albums.push(album);
-        album.position.z = -10;
-        //album.material.opacity = 0;
+        album.position.z = -50;
+        album.material.opacity = 0;
 
         this.scene.add(album);
+
+        anime({
+          targets: [album.position, album.material],
+          z: 0,
+          opacity: 1,
+          duration: 2000,
+          easing: 'easeOutQuad',
+        });
+
+        this.albums.add(album);
       });
+  }
+
+  /*
+  onResize = (event) => {
+    window.addEventListener('resize', this.onResize);
+    window.removeEventListener('resize', this.onResize);
+
+    const {camera} = this.props;
+    if (camera) {
+      const {offsetWidth, offsetHeight} = this.renderer.domElement;
+      console.log(this.renderer.domElement.getBoundingClientRect());
+      camera.aspect = offsetWidth / offsetHeight;
+      camera.updateProjectionMatrix();
+    }
+  }*/
+
+  update = (diff, total) => {
+    for (const album of this.albums) {
+      if (album.userData.update) {
+        album.userData.update(diff, total);
+      }
+    }
   }
 
   render() {
@@ -87,7 +131,7 @@ export class Album extends PureComponent {
           >
             {({ factor }) => (
               <Blur passes={4} factor={factor}>
-                <Renderer3D scene={this.scene} camera={this.camera} />
+                <Renderer3D size={resolution} scene={this.scene} camera={this.camera} onUpdate={this.update} />
               </Blur>
             )}
           </Motion>
