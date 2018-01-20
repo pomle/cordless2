ee1c82c868bf8895f95517611c303accf41ff187
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-20 17:16:27 +0100

    Remove track reducer.

--- src/layers/PlayerApplication/store/reducer.js
+++ src/layers/PlayerApplication/store/reducer.js
@@ -2,10 +2,8 @@ import { combineReducers } from 'redux';
 
 import { reducer as playlist } from './playlist';
 import { reducer as session } from './session';
-import { reducer as track } from './track';
 
 export const reducer = combineReducers({
   playlist,
   session,
-  track,
 });
