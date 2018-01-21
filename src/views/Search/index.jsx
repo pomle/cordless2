f93980c1149c4d12b29abff030cef58fd98c00b6
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-21 18:40:49 +0100

    Remove deprecated context from search.

--- src/views/Search/index.jsx
+++ src/views/Search/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 
 import { TrackList } from 'fragments/TrackList';
 import { Track } from 'fragments/Track';
@@ -11,14 +10,8 @@ import { search, playTracks } from '@pomle/spotify-redux';
 import './Search.css';
 
 export class Search extends Component {
-  static contextTypes = {
-    api: PropTypes.object,
-  };
-
-  constructor(props, context) {
+  constructor(props) {
     super(props);
-
-    this.playbackAPI = context.api.playbackAPI;
 
     this.handleSearchInput = debounce(this.handleSearchInput, 300);
 
