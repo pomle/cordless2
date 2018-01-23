195082effba0192519d4e1e1774e9c335495f61a
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-23 19:12:33 +0100

    Revert "Use API directly instead of Redux actions."
    This reverts commit 0ec2ddcc15192b144e900924026e1da2120c5957.

--- src/views/Playback/index.jsx
+++ src/views/Playback/index.jsx
@@ -8,46 +8,40 @@ import { NowPlaying } from './NowPlaying';
 import { Scrubber } from './Scrubber';
 import { Time } from 'components/Time';
 
+import { pause, resume, seek, next, prev } from '@pomle/spotify-redux';
+
 import './Playback.css';
 
 export class Playback extends Component {
   static propTypes = {
-    playbackAPI: PropTypes.object.isRequired,
+    pause: PropTypes.func.isRequired,
+    resume: PropTypes.func.isRequired,
+    seek: PropTypes.func.isRequired,
+    next: PropTypes.func.isRequired,
+    prev: PropTypes.func.isRequired,
   };
 
   toggle = () => {
-    const { playbackAPI: api, player: {context} } = this.props;
+    const { resume, pause, player: {context} } = this.props;
     if (context.paused) {
-      api.resume();
+      resume();
     } else {
-      api.pause();
+      pause();
     }
   };
 
-  next = () => {
-    this.props.playbackAPI.next();
-  }
-
-  prev = () => {
-    this.props.playbackAPI.prev();
-  }
-
-  seek = (ms) => {
-    this.props.playbackAPI.seek(ms);
-  };
-
   render() {
-    const { player, analysis } = this.props;
+    const { prev, next, seek, player, analysis } = this.props;
 
     return (
       <div className="Playback">
         <NowPlaying track={player.currentTrack} />
         <Scrubber
           context={player.context}
-          seek={this.seek}
+          seek={seek}
           analysis={analysis}
         />
-        <Interface prev={this.prev} next={this.next} toggle={this.toggle} />
+        <Interface prev={prev} next={next} toggle={this.toggle} />
         <div className="vis">
           <Link to="/now-playing">Now Playing</Link>
           <div className="time">
@@ -65,6 +59,7 @@ export default connect(state => {
   return {
     analysis: state.track.analysis.get(trackId),
     player: state.player,
-    playbackAPI: state.session.playbackAPI,
   };
+}, {
+  pause, resume, seek, next, prev
 })(Playback);
