0c448a376908a6e7fbdf3148826e4ea515114400
Pontus Persson <pontus@warpinmedia.com>
2018-01-29 17:16:44 +0100

    WIP Scrolldetect

--- src/layers/PlayerUI/index.jsx
+++ src/layers/PlayerUI/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 import { Navigation } from './Navigation';
 import Playback from 'views/Playback';
@@ -6,13 +7,52 @@ import Playback from 'views/Playback';
 import './Animation.css';
 import './PlayerUI.css';
 
+class Viewport extends Component {
+  static childContextTypes = {
+    scroll: PropTypes.object,
+    viewport: PropTypes.object,
+  };
+
+  componentDidMount() {
+    this.viewport.addEventListener('scroll', this.onScroll);
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    this.viewport.removeEventListener('scroll', this.onScroll);
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = (event) => {
+    console.log('Resize', event);
+  }
+
+  onScroll = (event) => {
+    //console.log('Scroll', event);
+    this.scroll = event;
+  }
+
+  getChildContext() {
+    return {
+      scroll: this.scroll,
+      viewport: this.viewport,
+    };
+  }
+
+  render() {
+    return <div className="viewport" ref={node => this.viewport = node}>
+      {this.props.children}
+    </div>;
+  }
+}
+
 export class PlayerUI extends Component {
   render() {
     return (
       <div className="PlayerUI">
-        <div className="viewport">
+        <Viewport>
           <Navigation />
-        </div>
+        </Viewport>
 
         <div className="playback">
           <Playback />
