214ecd0bd278171e0fa3ad931d77dde61124ed79
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-30 22:43:14 +0100

    Scroll experiments.

--- src/layers/PlayerUI/Navigation/index.jsx
+++ src/layers/PlayerUI/Navigation/index.jsx
@@ -4,10 +4,9 @@ import { withRouter, Link, Route, Switch } from 'react-router-dom';
 import { ViewContainer } from 'components/ViewContainer';
 
 import {AlbumRoute, ArtistRoute, PlaylistRoute, SearchRoute} from './Routes';
-import UserPlaylistsView from 'views/User/Playlists';
+import MyPlaylists from 'views/Me/MyPlaylists';
+//import UserPlaylistsView from 'views/User/Playlists';
 import { TrackInfo } from 'views/TrackInfo';
-
-import PlaylistAdapter from 'store/adapter/Playlist';
 
 import './Navigation.css';
 
@@ -21,9 +20,7 @@ export const Navigation = withRouter(class Navigation extends PureComponent {
             <Route path="/artist/:artistId" component={ArtistRoute} />
             <Route path="/user/:userId/playlist/:playlistId" component={PlaylistRoute} />
 
-            <Route path="/playlist2" component={PlaylistAdapter}/>
-
-            <Route exact path="/playlists" component={UserPlaylistsView}/>
+            <Route exact path="/playlists" component={MyPlaylists}/>
 
             <Route path="/search/:query?" component={SearchRoute}/>
 
