340cc8eeee91c3cca3547e13febfcbfd5dd91cc1
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-30 22:43:14 +0100

    Scroll experiments.

--- src/fragments/PlaylistList/index.jsx
+++ src/fragments/PlaylistList/index.jsx
@@ -2,23 +2,37 @@ import React, { Component } from 'react';
 
 import { PlayableList } from 'components/PlayableList';
 import { Playlist } from 'fragments/Playlist';
+import ViewportDetector from 'components/ViewportDetector';
 
 import './PlaylistList.css';
 
 export class PlaylistList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.seen = new Set();
+  }
+
   render() {
-    const { playlists, playbackAPI } = this.props;
+    const { playlists, playbackAPI, onViewportChange } = this.props;
+    const items = playlists.items;
 
     return (
       <div className="PlaylistList">
         <PlayableList>
-          {playlists.map(playlist => {
-            return (
-              <div key={playlist.get('id')} className="item">
+          <ViewportDetector count={playlists.total} onDraw={(index, visible) => {
+            if (!items.has(index) || (!this.seen.has(index) && !visible)) {
+              return <div key={`placeholder-${index}`} className="placeholder"/>;
+            }
+
+            this.seen.add(index);
+
+            const playlist = items.get(index);
+
+            return <div key={playlist.get('id') + '-' + index} className="item">
                 <Playlist playlist={playlist} playbackAPI={playbackAPI} />
-              </div>
-            );
-          })}
+            </div>
+          }}/>
         </PlayableList>
       </div>
     );
