fbd049ff8776c6d8ce863ccf00000d8937b48c7c
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-31 23:08:57 +0100

    WAT

--- src/fragments/PlaylistList/index.jsx
+++ src/fragments/PlaylistList/index.jsx
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
-
 import { PlayableList } from 'components/PlayableList';
-import { Playlist } from 'fragments/Playlist';
-import ViewportDetector from 'components/ViewportDetector';
 
 import './PlaylistList.css';
 
 export class PlaylistList extends Component {
   render() {
-    const { playlists, playbackAPI, onViewportChange } = this.props;
-    const items = playlists.items;
-
     return (
       <div className="PlaylistList">
         <PlayableList>
-          <ViewportDetector count={playlists.total} onDraw={index => {
-            if (!items.has(index)) {
-              return null;
-            }
-
-            const playlist = items.get(index);
-            return <div key={playlist.get('id') + '-' + index} className="item">
-                <Playlist playlist={playlist} playbackAPI={playbackAPI} />
-            </div>
-          }}/>
+          {this.props.children}
         </PlayableList>
       </div>
     );
