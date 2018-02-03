89ee5c46e3e8ef2cdac3adb1bf6cb22bb000dafe
Pontus Alexander <pontus.alexander@gmail.com>
2018-02-03 18:26:05 +0100

    Collection stash in Redux.

--- src/store/store/stash/index.js
+++ src/store/store/stash/index.js
@@ -1,11 +1,17 @@
 import { Record, Map, List, fromJS } from 'immutable';
 
-const ItemSet = Record({
+const Container = Record({
+  ready: false,
+  content: null,
+});
+
+const Collection = Record({
   total: null,
   items: new List(),
 });
 
-const DEFAULT = new ItemSet();
+const DEFAULT_COLLECTION = new Collection();
+const EMPTY = new Container();
 
 class State {
   constructor(data = new Map()) {
@@ -13,7 +19,7 @@ class State {
   }
 
   get(key) {
-    return this.data.get(key, DEFAULT);
+    return this.data.get(key, DEFAULT_COLLECTION);
   }
 
   set(key, value) {
@@ -22,6 +28,7 @@ class State {
 }
 
 const ADD_ITEMS = 'r/stash/add-items';
+const RESERVE_ITEMS = 'r/stash/reserve-items';
 const SET_TOTAL = 'r/stash/set-total';
 
 export function setTotal(key, total) {
@@ -41,6 +48,15 @@ export function addItems(key, offset, items) {
   };
 }
 
+export function reserveItems(key, offset, limit) {
+  return {
+    type: RESERVE_ITEMS,
+    key,
+    offset,
+    limit,
+  };
+}
+
 function update(state, key, updater) {
   return state.set(key, updater(state.get(key)));
 }
@@ -55,8 +71,21 @@ export function reducer(state = new State(), action = {}) {
         return stash.set('items', stash.items.withMutations(items => {
           action.items.forEach((item, index) => {
             const position = index + action.offset;
-            items.set(position, fromJS(item));
+            items.setIn([position, 'content'], fromJS(item));
+            items.setIn([position, 'ready'], true);
           });
+          return items;
+        }));
+      });
+
+    case RESERVE_ITEMS:
+      return update(state, action.key, stash => {
+        return stash.set('items', stash.items.withMutations(items => {
+          const start = action.offset;
+          const end = start + action.limit;
+          for (let position = start; position < end; position++) {
+            items.set(position, EMPTY);
+          };
           return items;
         }));
       });
