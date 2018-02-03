4109dc1b074c86fc80a23c213c1194b5887ba02f
Pontus Alexander <pontus.alexander@gmail.com>
2018-02-03 22:02:56 +0100

    WTF react mess.

--- src/views/User/Playlists/index.jsx
+++ src/views/User/Playlists/index.jsx
@@ -1,47 +1,29 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
+import UserPlaylistsMount from 'store/mounts/UserPlaylists';
 import { PlaylistList } from 'fragments/PlaylistList';
 import { Playlist } from 'fragments/Playlist';
 
 import Yxa from 'components/Yxa';
 
-import {fetchUserPlaylists} from 'store/store/userPlaylists';
-
 class UserPlaylistsView extends Component {
-  fetch(userId) {
-    return (offset, limit) => {
-      const {fetchUserPlaylists, collection} = this.props;
-      if (collection.items.get(offset)) {
-        return;
-      }
-
-      fetchUserPlaylists(userId, offset, limit);
-    }
-  }
-
   render() {
-    const {userId, collection} = this.props;
-    console.log(collection);
+    const {userId} = this.props;
 
-    return (
-      <PlaylistList>
-        <Yxa
-          collection={collection}
-          fetcher={this.fetch(userId)}
-          placeholder={<div className="playlist-container"><Playlist /></div>}
-          render={playlist => {
-            return <div className="playlist-container">
-              <Playlist playlist={playlist} />
-            </div>
-          }}
-        />
-      </PlaylistList>
-    );
+    return <UserPlaylistsMount userId={userId} render={({collection, fetcher}) => (
+        <PlaylistList>
+          <Yxa
+            collection={collection}
+            fetcher={fetcher}
+            placeholder={<div className="playlist-container"><Playlist /></div>}
+            render={playlist => {
+              return <div className="playlist-container">
+                <Playlist playlist={playlist} />
+              </div>
+            }}
+          />
+        </PlaylistList>
+    )}/>;
   }
 }
 
-export default connect(
-  (state, props) => ({collection: state.userPlaylists.get(props.userId)}),
-  {fetchUserPlaylists}
-)(UserPlaylistsView);
+export default UserPlaylistsView;
