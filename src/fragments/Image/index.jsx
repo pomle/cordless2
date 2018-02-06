13e7276ba0b7331bbfffb7d635494e596c5c2a15
Pontus Persson <pontus@warpinmedia.com>
2018-02-06 13:57:05 +0100

    Image element prefers medium size.

--- src/fragments/Image/index.jsx
+++ src/fragments/Image/index.jsx
@@ -1,6 +1,9 @@
 import React, { PureComponent } from 'react';
 import './Image.css';
 
+const BIG = 640;
+const MEDIUM = 300;
+
 export class Image extends PureComponent {
   render() {
     const {candidates} = this.props;
@@ -8,7 +11,10 @@ export class Image extends PureComponent {
       return null;
     }
 
-    const url = candidates.getIn([0, 'url']);
+    const images = candidates.groupBy(image => image.get('width'));
+    const image = images.getIn([MEDIUM, 0]) || images.getIn([BIG, 0]) || candidates.get(0);
+
+    const url = image && image.get('url');
     return <ImageSurface key={url} url={url}/>
   }
 }
