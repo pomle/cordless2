b3658490271d1220e66d7c96d81efece160c07df
Pontus Alexander <pontus.alexander@gmail.com>
2018-02-07 10:34:14 +0100

    Add playback icons.

---
+++ src/views/Playback/Interface/assets/icons.js
@@ -0,0 +1,15 @@
+import next from './next.svg';
+import prev from './prev.svg';
+import play from './play.svg';
+import pause from './pause.svg';
+import repeatAll from './repeat-all.svg';
+import shuffle from './shuffle.svg';
+
+export {
+    next,
+    prev,
+    play,
+    pause,
+    repeatAll,
+    shuffle,
+}
