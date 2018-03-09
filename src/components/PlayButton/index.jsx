dca7d57ee37fb582347b76d951c87e27b94c496b
Pontus Persson <pontus@warpinmedia.com>
2018-03-09 16:30:24 +0100

    WIP

--- src/components/PlayButton/index.jsx
+++ src/components/PlayButton/index.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import * as icon from 'assets/icons';
 import './PlayButton.css';
+
+const style = {
+    backgroundImage: `url(${icon.play})`,
+};
 
 export class PlayButton extends Component {
   render() {
     return (
-      <button className="PlayButton" onClick={this.props.onClick}>
+      <button className="PlayButton" onClick={this.props.onClick} style={style}>
         Play
       </button>
     );
