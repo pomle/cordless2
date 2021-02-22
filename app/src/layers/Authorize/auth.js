e97d5930c02264c8a982fd4e4534394ab9857b7d
Pontus Alexander <pontus.alexander@gmail.com>
2021-02-22 17:20:55 +0100

    Setup API hosting

--- app/src/layers/Authorize/auth.js
+++ app/src/layers/Authorize/auth.js
@@ -1,41 +1,37 @@
-import { parse } from 'query-string';
+import { parse } from "query-string";
 
-const SESSION_STORAGE_KEY = 'session2';
-const AUTH_SERVER = process.env.REACT_APP_AUTH_SERVER;
+const SESSION_STORAGE_KEY = "session2";
+const AUTH_URL = process.env.REACT_APP_AUTH_URL;
 
 const SCOPE = [
-  'user-read-private',
-  'user-read-playback-state',
-  'user-modify-playback-state',
-  'playlist-read-private',
-  'playlist-read-collaborative',
-  'streaming',
+  "user-read-private",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "streaming",
 ];
 
 function createImplicitFlowURL(clientId, callbackURL) {
   return (
-    'https://accounts.spotify.com/authorize?' +
+    "https://accounts.spotify.com/authorize?" +
     [
-      ['client_id', clientId],
-      ['redirect_uri', callbackURL],
-      ['response_type', 'token'],
-      [
-        'scope',
-        SCOPE.join(' '),
-      ],
+      ["client_id", clientId],
+      ["redirect_uri", callbackURL],
+      ["response_type", "token"],
+      ["scope", SCOPE.join(" ")],
     ]
-    .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-    .join('&')
+      .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+      .join("&")
   );
 }
 
 export function createAuthURL() {
-  if (AUTH_SERVER) {
-    return AUTH_SERVER + '/login?scope=' + SCOPE.join(' ');
+  if (AUTH_URL) {
+    return AUTH_URL + "/login?scope=" + SCOPE.join(" ");
   } else {
     const CLIENT_ID = "a7cf3dcdfbd64bd5ac8d960caabbc890";
-    const CALLBACK_URL =
-      process.env.REACT_APP_SITE_URL || window.location.hostname;
+    const CALLBACK_URL = window.location.hostname;
     return createImplicitFlowURL(CLIENT_ID, CALLBACK_URL);
   }
 }
@@ -65,16 +61,19 @@ export function purgeSession(storage) {
 }
 
 export function canRefresh() {
-  return !!AUTH_SERVER;
+  return !!AUTH_URL;
 }
 
 export function refreshToken(refreshTokenThatIsNotAccessToken) {
   if (!canRefresh()) {
-    throw new Error('Can not refresh without authentication server');
+    throw new Error("Can not refresh without authentication server");
   }
 
-  const url = AUTH_SERVER + '/refresh_token?refresh_token=' + refreshTokenThatIsNotAccessToken;
-  return fetch(url).then(response => response.json());
+  const url =
+    AUTH_URL +
+    "/refresh_token?refresh_token=" +
+    refreshTokenThatIsNotAccessToken;
+  return fetch(url).then((response) => response.json());
 }
 
 export function parseQueryString(qs) {
