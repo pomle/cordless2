7895abd6c23a8ac547e258ad7dcb5b6a80c1a85f
Pontus Alexander <pontus.alexander@gmail.com>
2021-02-22 17:20:55 +0100

    Setup API hosting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,10 @@
 import * as functions from "firebase-functions";
+import * as express from "express";
+import * as cors from "cors";
 import auth from "./auth";
 
-exports.auth = functions.https.onRequest(auth);
+const app = express();
+app.use(cors());
+app.use("/auth", auth);
+
+exports.api = functions.https.onRequest(app);
