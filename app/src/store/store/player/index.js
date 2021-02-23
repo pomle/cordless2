8a36bf32aa87023b42e89e8277d3244d78e87a1e
Pontus Alexander <pontus.alexander@gmail.com>
2021-02-23 14:59:38 +0100

    Format file

--- app/src/store/store/player/index.js
+++ app/src/store/store/player/index.js
@@ -1,4 +1,4 @@
-import { Record } from 'immutable';
+import { Record } from "immutable";
 
 const Context = Record({
   bitrate: null,
@@ -25,60 +25,56 @@ const PlayerState = Record({
 
 class Player extends PlayerState {
   get currentTrack() {
-    return this.getIn(['context', 'track_window', 'current_track']);
+    return this.getIn(["context", "track_window", "current_track"]);
   }
 }
 
-const MESSAGE = 'r/player/message';
+const MESSAGE = "r/player/message";
 
 function withAPI(fn) {
   return (dispatch, getState) => {
-    const {session, player} = getState();
+    const { session, player } = getState();
     const api = session.playbackAPI;
     api.setDevice(player.deviceId);
     fn(api, player);
-  }
+  };
 }
 
 function wrapAPI(fnName) {
-  return (...args) => withAPI(api => api[fnName](...args));
+  return (...args) => withAPI((api) => api[fnName](...args));
 }
 
-export const playAlbum = wrapAPI('playAlbum');
-export const playContext = wrapAPI('playContext');
-export const playPlaylist = wrapAPI('playPlaylist');
-export const playTracks = wrapAPI('playTracks');
+export const playAlbum = wrapAPI("playAlbum");
+export const playContext = wrapAPI("playContext");
+export const playPlaylist = wrapAPI("playPlaylist");
+export const playTracks = wrapAPI("playTracks");
 
-export const seek = wrapAPI('seek');
-export const pause = wrapAPI('pause');
-export const resume = wrapAPI('resume');
-export const next = wrapAPI('next');
-export const prev = wrapAPI('prev');
-export const repeat = wrapAPI('repeat');
-export const shuffle = wrapAPI('shuffle');
-export const volume = wrapAPI('volume');
+export const seek = wrapAPI("seek");
+export const pause = wrapAPI("pause");
+export const resume = wrapAPI("resume");
+export const next = wrapAPI("next");
+export const prev = wrapAPI("prev");
+export const repeat = wrapAPI("repeat");
+export const shuffle = wrapAPI("shuffle");
+export const volume = wrapAPI("volume");
 
-const REPEAT_CYCLE = [
-  'off',
-  'context',
-  'track',
-];
+const REPEAT_CYCLE = ["off", "context", "track"];
 
 export function cycleRepeat() {
-  return withAPI((api, {context}) => {
+  return withAPI((api, { context }) => {
     const next = (context.repeat_mode + 1) % REPEAT_CYCLE.length;
     api.repeat(REPEAT_CYCLE[next]);
   });
 }
 
 export function cycleShuffle() {
-  return withAPI((api, {context}) => {
+  return withAPI((api, { context }) => {
     api.shuffle(!context.shuffle);
   });
 }
 
 export function cyclePlayback() {
-  return withAPI((api, {context}) => {
+  return withAPI((api, { context }) => {
     if (context.paused) {
       api.resume();
     } else {
@@ -96,21 +92,21 @@ export function handleMessage(name, message) {
 }
 
 function toId(uri) {
-  return uri.split(':')[2];
+  return uri.split(":")[2];
 }
 
 function mergeContext(state, context) {
   for (const artist of context.track_window.current_track.artists) {
     artist.id = toId(artist.uri);
   }
-  return state.mergeIn(['context'], context);
+  return state.mergeIn(["context"], context);
 }
 
 function onMessage(state, name, message) {
   switch (name) {
-    case 'ready':
-      return state.set('ready', true).set('deviceId', message.device_id);
-    case 'state':
+    case "ready":
+      return state.set("ready", true).set("deviceId", message.device_id);
+    case "state":
       return mergeContext(state, message);
     default:
       return state;
@@ -118,7 +114,7 @@ function onMessage(state, name, message) {
 }
 
 export function reducer(state = new Player(), action = {}) {
-  switch(action.type) {
+  switch (action.type) {
     case MESSAGE:
       return onMessage(state, action.name, action.message);
     default:
