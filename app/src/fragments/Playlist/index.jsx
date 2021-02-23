11650ec7f16eff19eafcf5418f1b04c2cdbb861d
Pontus Alexander <pontus.alexander@gmail.com>
2021-02-23 15:25:52 +0100

    Make playlist image clickable

--- app/src/fragments/Playlist/index.jsx
+++ app/src/fragments/Playlist/index.jsx
@@ -1,44 +1,50 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-import { PlayButton } from 'components/PlayButton';
-import { Image } from 'fragments/Image';
+import { PlayButton } from "components/PlayButton";
+import { Image } from "fragments/Image";
 
-import { playContext } from 'store';
+import { playContext } from "store";
 
-import './Playlist.css';
+import "./Playlist.css";
 
-export const Playlist = connect(null, {playContext})(class Playlist extends PureComponent {
-  play = () => {
-    const { playlist, playContext } = this.props;
-    playContext(playlist.get('uri'));
-  };
+export const Playlist = connect(null, { playContext })(
+  class Playlist extends PureComponent {
+    play = () => {
+      const { playlist, playContext } = this.props;
+      playContext(playlist.get("uri"));
+    };
 
-  render() {
-    const { playlist } = this.props;
-    const owner = playlist.get('owner');
+    render() {
+      const { playlist } = this.props;
+      const owner = playlist.get("owner");
 
-    return (
-      <div className="Playlist">
-        <div className="image">
-          <Image candidates={playlist.get('images')} />
-        </div>
+      const playlistURL = `/user/${owner.get("id")}/playlist/${playlist.get(
+        "id"
+      )}`;
 
-        <div className="name">
-          <Link to={`/user/${owner.get('id')}/playlist/${playlist.get('id')}`}>
-            {playlist.get('name')}
+      return (
+        <div className="Playlist">
+          <Link component="div" to={playlistURL} className="image">
+            <Image candidates={playlist.get("images")} />
           </Link>
-        </div>
 
-        <div className="owner">
-          <Link to={`/user/${owner.get('id')}`}>{owner.get('display_name')}</Link>
-        </div>
+          <div className="name">
+            <Link to={playlistURL}>{playlist.get("name")}</Link>
+          </div>
 
-        <div className="playback">
-          <PlayButton onClick={this.play} />
+          <div className="owner">
+            <Link to={`/user/${owner.get("id")}`}>
+              {owner.get("display_name")}
+            </Link>
+          </div>
+
+          <div className="playback">
+            <PlayButton onClick={this.play} />
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-});
+);
