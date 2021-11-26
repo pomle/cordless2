18202d6c8c089ead4750ebc4683746ae9002494e
Pontus Alexander <pontus.alexander@gmail.com>
2021-11-26 22:03:24 +0100

    Fix playlist filter

--- app/src/library/search.js
+++ app/src/library/search.js
@@ -1,20 +1,21 @@
 export function matcher(needle) {
   needle = needle.toLowerCase();
   return function match(...haystack) {
-    return haystack.some(word => word.toString().toLowerCase().includes(needle));
+    return haystack.some((word) =>
+      word.toString().toLowerCase().includes(needle)
+    );
   };
 }
 
 export function matchPlaylist(playlist, match) {
-  return match([
-    playlist.get('name'),
-  ]);
+  return match([playlist.get("name")]);
 }
 
 export function matchTrack(track, match) {
-  return match([
-    track.get('name'),
-    ...track.get('artists').map(artist => artist.get('name')),
-    track.getIn('album','name'),
-  ]);
+  const subjects = [
+    track.get("name"),
+    ...track.get("artists").map((artist) => artist.get("name")),
+    track.get("album").get("name"),
+  ];
+  return match(subjects);
 }
