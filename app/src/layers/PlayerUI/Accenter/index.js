78df557e246c2edb56d6389ebf5ef96854f3f857
Pontus Alexander <pontus.alexander@gmail.com>
2021-11-28 20:54:10 +0100

    Apply color on Playerbar

--- app/src/layers/PlayerUI/Accenter/index.js
+++ app/src/layers/PlayerUI/Accenter/index.js
@@ -4,15 +4,46 @@ import { connect } from "react-redux";
 function getColor(color) {
   if (color) {
     if (color.vibrant) {
-      return `rgb(${color.vibrant.rgb.join(",")})`;
+      return toCSS(color.vibrant.rgb, 1);
     }
 
     if (color.lightvibrant) {
-      return `rgb(${color.lightvibrant.rgb.join(",")})`;
+      return toCSS(color.lightvibrant.rgb, 1);
     }
   }
 
   return undefined;
+}
+
+function getMutedColor(color) {
+  if (color) {
+    if (color.darkvibrant) {
+      return toCSS(
+        color.darkvibrant.rgb.map((v) => v * 0.2),
+        0.9
+      );
+    }
+
+    if (color.muted) {
+      return toCSS(
+        color.muted.rgb.map((v) => v * 0.1),
+        0.9
+      );
+    }
+
+    if (color.darkmuted) {
+      return toCSS(
+        color.darkmuted.rgb.map((v) => v * 0.2),
+        0.9
+      );
+    }
+  }
+
+  return undefined;
+}
+
+function toCSS(rgb, alpha = 1) {
+  return `rgb(${rgb.join(",")}, ${alpha})`;
 }
 
 export const Accenter = connect((state) => {
@@ -26,6 +57,7 @@ export const Accenter = connect((state) => {
   class Accenter extends Component {
     render() {
       const accentColor = getColor(this.props.color);
+      const backgroundColor = getMutedColor(this.props.color);
       console.log(this.props.color);
 
       return (
@@ -42,6 +74,14 @@ export const Accenter = connect((state) => {
 
                 .ProgressBar .progress {
                     ${accentColor ? `background-color: ${accentColor};` : ""}
+                }
+
+                .PlayerWindow .Playback {
+                    ${
+                      backgroundColor
+                        ? `background-color: ${backgroundColor};`
+                        : ""
+                    }
                 }
             `,
           }}
